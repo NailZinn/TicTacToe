ea0f8f5cdf0afdaff626e4dd51e0d369fe53d4fa
NailZinn <nailzinnurov17@gmail.com>
2024-01-25 16:47:37 +0300

    added mocked game board

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,7 @@ import Authentication from './pages/authentication/authentication-page';
 import Authorization from './pages/authorization/authorization-page';
 import MainPage from './pages/main/main-page';
 import Ratings from './pages/ratings/ratings-page';
+import Game from './pages/game/game-page';
 
 const App = () => {
 
@@ -15,6 +16,7 @@ const App = () => {
                 <Route path='/authentication' element={ <Authentication /> } />
                 <Route path='/authorization' element={ <Authorization /> } />
                 <Route path='/ratings' element={ <Ratings /> } />
+                <Route path='/game/:id' element={ <Game /> }/>
             </Routes>
         </Layout>
     )
