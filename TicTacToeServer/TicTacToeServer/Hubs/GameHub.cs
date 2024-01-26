42be9db41aeabe95be9c1807cc2bf6f444cf91f1
NailZinn <nailzinnurov17@gmail.com>
2024-01-26 19:51:04 +0300

    fixed game messages

--- TicTacToeServer/TicTacToeServer/Hubs/GameHub.cs
+++ TicTacToeServer/TicTacToeServer/Hubs/GameHub.cs
@@ -57,8 +57,8 @@ public class GameHub : Hub<IGameHubClient>
 
             var currentPlayerSymbolIndex = random.Next(2);
             var currentPlayerTurn = random.Next(2) == 0;
-            var messageForSender = new StartGameMessage(symbols[currentPlayerSymbolIndex], currentPlayerTurn, GameMessages[gameId]);
-            var messageForReceiver = new StartGameMessage(symbols[1 - currentPlayerSymbolIndex], !currentPlayerTurn, GameMessages[gameId]);
+            var messageForSender = new StartGameMessage(symbols[currentPlayerSymbolIndex], currentPlayerTurn, GameMessages.GetValueOrDefault(gameId) ?? []);
+            var messageForReceiver = new StartGameMessage(symbols[1 - currentPlayerSymbolIndex], !currentPlayerTurn, GameMessages.GetValueOrDefault(gameId) ?? []);
             
             var setGameStatus = new SetGameStateCommand(int.Parse(gameId), GameStatus.Started);
             await _mediator.Send(setGameStatus);
@@ -71,7 +71,7 @@ public class GameHub : Hub<IGameHubClient>
         {
             var getGameField = new GetGameFieldQuery(int.Parse(gameId));
             var gameField = await _mediator.Send(getGameField);
-            await Clients.Clients(UserConnections[GetUserId()]).ReceiveWatcherMessageAsync(gameField!, GameMessages[gameId]);
+            await Clients.Clients(UserConnections[GetUserId()]).ReceiveWatcherMessageAsync(gameField!, GameMessages.GetValueOrDefault(gameId) ?? []);
         }
         else
         {
